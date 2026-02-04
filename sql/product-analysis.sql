25029079dbc771e500bde9156b092abadea3518c
jmateovill <jmateo.vill@gmail.com>
2026-02-05 00:00:46 +0800

    Added queries on product analysis

--- sql/product-analysis.sql
+++ sql/product-analysis.sql
@@ -31,3 +31,53 @@ SELECT
 FROM product_ranking
 WHERE revenue_ranking <= 10
 ORDER BY revenue_ranking;
+
+-- top product per month by total revenue 
+WITH product AS (
+	SELECT 
+		strftime("%Y-%m", InvoiceDate) as month,
+		Description as product_sold, 
+		Revenue as revenue
+	FROM retail
+),
+
+revenue_per_product AS (
+	SELECT 
+		month,
+		product_sold,
+		sum(revenue) as total_revenue
+	FROM product 
+	GROUP BY product_sold, month
+)
+
+SELECT 
+	month,
+	product_sold as top_product,
+	max(total_revenue) as top_product_revenue
+FROM revenue_per_product
+GROUP BY month;
+
+-- top product per month by units sold
+WITH product AS (
+	SELECT 
+		strftime("%Y-%m", InvoiceDate) as month,
+		Description as product_sold, 
+		Quantity as units_sold
+	FROM retail
+),
+
+units_sold_per_product AS (
+	SELECT 
+		month,
+		product_sold,
+		sum(units_sold) as total_units_sold
+	FROM product 
+	GROUP BY product_sold, month
+)
+
+SELECT 
+	month,
+	product_sold as top_product,
+	max(total_units_sold) as top_product_units_sold
+FROM units_sold_per_product
+GROUP BY month;
